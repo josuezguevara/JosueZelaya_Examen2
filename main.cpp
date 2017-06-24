67ece5f00ed0e2fc943e0f3ce6bb789134e7c5c4
Josuezguevara <josuezelaya99@hotmail.es>
2017-06-23 18:26:14 -0600

    Entra a JUgar

--- main.cpp
+++ main.cpp
@@ -259,7 +259,8 @@ int main(){
                     case 1:{//jugar
                       for (int j = 0; j < mesas.size(); j++) {
                         if (personas[i]==mesas[j]->getJugador()){//el jugador juega en la mesa puesta
-
+                          string dinero_jugador;
+                          dinero_jugador=personas[i]->getDinero();
                         }
                       }//fin for mesa
                       break;
